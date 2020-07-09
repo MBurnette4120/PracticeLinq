8a41274b0759bc07e806207053bdeeb258a37a50
MBurnette4120 <mikeburnette@rocketmail.com>
2020-07-09 15:15:37 -0400

    Add project files.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] carNames = new[] { "Chevrolet", "Ford", "Acura", "Nissan" };
+
+            var namesOfCars = from cars in carNames
+                              where cars.Contains("r")
+                              orderby cars descending
+                              select cars;
+            foreach (var i in namesOfCars)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            List <string>  videoGames = new List <string> {"Call of Duty", "Roblox", "Fortnite", "Wolfenstein" };
+
+            IEnumerable<string> myGames = videoGames.OrderByDescending(game => (game.Length));
+
+            foreach (var i in myGames)
+            {
+                Console.WriteLine(i);
+            }
+
+            
+
+
+        }
+    }
+}
